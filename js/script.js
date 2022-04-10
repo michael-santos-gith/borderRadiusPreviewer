6266dc9f8579577446368e1e6eff1ee2037c112c
michael-santos-gith <maiconxandroid@gmail.com>
2022-04-09 21:50:59 -0300

    Refatorado o resetar e copiar do script.js.

--- js/script.js
+++ js/script.js
@@ -1,41 +1,45 @@
 let contentBeModificate = document.getElementById("content-modify");
 let inputFocuSelect = document.querySelectorAll('.input-click');
-let textAreaContent = document.getElementById('result-borders');
 let buttonCopyReset = document.querySelectorAll('.button-choose');
-let resetAll = document.getElementById('reset-all');
-let copyTextArea = document.getElementById('copy-text-area');
+let textAreaContent = document.getElementById('result-borders');
 
 function inputFocus(amount, indicator) {
     inputFocuSelect[indicator].addEventListener('input', function() {
         if (indicator == 0) {
             contentBeModificate.style.borderTopLeftRadius = amount.value + 'px';
+            textAreaContent.value = `border-top-left-radius: ${amount.value}px;`;
         } else if (indicator == 1) {
             contentBeModificate.style.borderTopRightRadius = amount.value + 'px';
+            textAreaContent.value = `border-top-right-radius: ${amount.value}px;`;
         } else if (indicator == 2) {
             contentBeModificate.style.borderBottomRightRadius = amount.value + 'px';
-        } else {{
+            textAreaContent.value = `border-bottom-right-radius: ${amount.value}px;`;
+        } else {
             contentBeModificate.style.borderBottomLeftRadius = amount.value + 'px';
-        }}
+            textAreaContent.value = `border-bottom-left-radius: ${amount.value}px;`;
+        }
     });
 }
-// APLICA O BORDER-RADIUS 
+
+function resetAndCopyButtons(amount, indicator) {
+    buttonCopyReset[indicator].addEventListener('click', function() {
+        if (indicator == 0) {
+            inputFocuSelect.forEach(function(content) {
+                content.value = '';
+                textAreaContent.value = '';
+                contentBeModificate.style.borderTopLeftRadius = 0 + 'px';
+                contentBeModificate.style.borderTopRightRadius = 0 + 'px';
+                contentBeModificate.style.borderBottomRightRadius = 0 + 'px';
+                contentBeModificate.style.borderBottomLeftRadius = 0 + 'px';
+            });
+        } else {
+            textAreaContent.select();
+            textAreaContent.setSelectionRange(0, 9999);
+
+            navigator.clipboard.writeText(textAreaContent.value);
+        }
+    })
+}
+
 inputFocuSelect.forEach(inputFocus);
-
-resetAll.addEventListener('click', function() {
-    leftUpSide.value = '';
-    rightUpSide.value = '';
-    rightDownSide.value = '';
-    leftDownSide.value = '';
-    textAreaContent.value = '';
-    contentBeModificate.style.borderTopLeftRadius = leftUpSide.value = 0 + 'px';
-    contentBeModificate.style.borderTopRightRadius = rightUpSide.value = 0 + 'px';
-    contentBeModificate.style.borderBottomRightRadius = rightDownSide.value = 0 + 'px';
-    contentBeModificate.style.borderBottomLeftRadius = leftDownSide.value = 0 + 'px';
-})
-
-copyTextArea.addEventListener('click', function() {
-    textAreaContent.select();
-    textAreaContent.setSelectionRange(0, 9999);
-
-    navigator.clipboard.writeText(textAreaContent.value);
-})
+buttonCopyReset.forEach(resetAndCopyButtons);
